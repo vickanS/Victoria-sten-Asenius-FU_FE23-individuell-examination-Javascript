5cc9fa02a8a63f60afe28141d5e228c1f6782f22
vickanS <149102936+vickanS@users.noreply.github.com>
2024-02-20 19:08:26 +0100

    added functionality to the searchfield and the searchbutton

--- index.js
+++ index.js
@@ -1,11 +1,21 @@
-async function fetchData() {
-    let resp = await fetch('https://n5n3eiyjb0.execute-api.eu-north-1.amazonaws.com/bodies', {
-      method: 'GET',
-      headers: {'x-zocom': 'solaris-2ngXkR6S02ijFrTP'}
-    });
-  
-    let data = await resp.json();
-    console.log(data);
-  }
-  
-  fetchData();
+document.getElementById('searchbtn').addEventListener('click', function(event) {  
+    event.preventDefault(); 
+
+    async function fetchData() {
+        let resp = await fetch('https://n5n3eiyjb0.execute-api.eu-north-1.amazonaws.com/bodies', {
+          method: 'GET',
+          headers: {'x-zocom': 'solaris-2ngXkR6S02ijFrTP',
+          'cache-control': 'no-cache'
+        }
+        });
+      
+        let data = await resp.json();
+        let searchValue = document.getElementById('searchfield').value;
+        let filteredData = data.bodies.filter(function(body) {
+          return body.name.toLowerCase() === searchValue.toLowerCase();
+        });
+        console.log(filteredData);
+      }
+      
+      fetchData().catch(error => console.error(error));
+});
